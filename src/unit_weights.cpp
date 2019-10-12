17b29d6af297bb3df2b7de1018841703d877e70c
longxunfenger <longxunfenger@sina.com>
2019-10-12 20:37:42 +0800

    test

--- src/unit_weights.cpp
+++ src/unit_weights.cpp
@@ -7,7 +7,7 @@
 //' The function find optimal unit weights
 //' @param Y00 - T0 x N0 matrix of control unit outcomes before the treatment
 //' @param Y10 - T0 x N - N0 matrix of treated unit outcomes before the treatment
-//, @param N_treated_units - the number of treated units
+//, @param N1 - the number of treated units
 //' @param dzeta - Ridge penalty parameter
 //' @param rho - parameter used in ADMM
 //' @param tol - Convergence tolerance for ADMM algorithm
@@ -15,17 +15,18 @@
 //' @return N x 1 matrix of time weights for the estimation
 //' @export
 // [[Rcpp::export]]
-arma::mat unit_weights(arma::mat &Y00, arma::mat &Y10, int N_treated_units, double dzeta, double rho, double tol, std::string weight_type)
+arma::mat unit_weights(arma::mat &Y00, arma::mat &Y10, int N1, double dzeta, double rho, double tol, std::string weight_type)
 {
   int T0 = Y00.n_rows;
-  int N = Y00.n_cols + 1;
-  int N_units = Y00.n_cols + N_treated_units;
-  arma::mat unit_weights(N - 1, 1);
+  int N0 = Y00.n_cols;
+  int N = N0 + N1;
+  //int N_units = Y00.n_cols + N1;
+  arma::mat unit_weights(N0, 1);
   
-  arma::mat eye_mat(N - 1, N - 1);
+  arma::mat eye_mat(N0, N0);
   eye_mat.eye();
 
-  arma::mat V(N - 1, N - 1);
+  arma::mat V(N0, N0);
 
   V = Y00.t() * Y00/T0 + dzeta*eye_mat;
   //arma::colvec dvec = - Y10.t() * Y00/T0;
@@ -40,10 +41,10 @@ arma::mat unit_weights(arma::mat &Y00, arma::mat &Y10, int N_treated_units, doub
     //unit_weights = admm_solver(V, dvec, rho, 10000, 1e-8);
     
     
-    arma::mat result(N_units, 1);
-    result.zeros();
-    //arma::mat treated_weights(N_treated_units, 1);
-    result.submat(0, 0, N - 2, 0) = admm_solver(V, dvec, rho, 10000, 1e-8);
+    //arma::mat result(N, 1);
+    arma:mat result(N, 1, arma::fill::zeros)
+    //arma::mat treated_weights(N1, 1);
+    result.submat(0, 0, N0 - 1, 0) = admm_solver(V, dvec, rho, 10000, 1e-8);
     //result.submat(N - 1, 0, N_units - 1, 0) = treated_weights.fill(0);
     
     return result;
--- src/unit_weights.h
+++ src/unit_weights.h
@@ -3,5 +3,5 @@
 #include <string>
 
 // [[Rcpp::depends("RcppArmadillo")]]
-arma::mat unit_weights(arma::mat &Y00, arma::mat &Y10, int N_treated_units, double dzeta, double rho = 1, double tol = 1e-8, std::string weight_type = "SDID");
+arma::mat unit_weights(arma::mat &Y00, arma::mat &Y10, int N1, double dzeta, double rho = 1, double tol = 1e-8, std::string weight_type = "SDID");
   
