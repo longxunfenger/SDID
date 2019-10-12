c2dcd676dd30d042f241aaa6c1626513d0b6868a
longxunfenger <longxunfenger@sina.com>
2019-10-12 19:38:11 +0800

    for multiple treated units -test

--- src/unit_weights.cpp
+++ src/unit_weights.cpp
@@ -43,7 +43,7 @@ arma::mat unit_weights(arma::mat &Y00, arma::mat &Y10, int N_treated_units, doub
     arma::mat result(N_units, 1);
     arma::mat treated_weights(N_treated_units, 1);
     result.submat(0, 0, N - 2, 0) = admm_solver(V, dvec, rho, 10000, 1e-8);
-    result.submat(N - 1, 0, N_units - 1, 0) = treated_weights.fill(1/N_treated_units);
+    // result.submat(N - 1, 0, N_units - 1, 0) = treated_weights.fill(1 / N_treated_units);
     
     return result;
     //return unit_weights;
